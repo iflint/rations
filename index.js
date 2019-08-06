80fb912b12f5a9971a85698d6174b9faaed8d616
ianeflint <ian.flint@icloud.com>
2019-08-05 20:16:15 -0400

    operational collections page

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ const pool = new Pool({
 
 app.use(express.static(__dirname+'/public'))
 
+var allCollections = {
+	highProtein : [10182, 22118, 1001]
+}
+
 app.get('/', (req, res) => res.sendFile('/index.html', {root: __dirname}))
 app.get('/search/:itemId', (req, res) => res.sendFile('/search.html', {root: __dirname}))
 app.get('/collections/:collectionId', (req, res) => res.sendFile('/collection.html', {root: __dirname}))
@@ -27,6 +31,19 @@ app.get('/api/search/:searchWord', (req, res) => {
 
 	// searchWord must be enclosed in single quotes
 	var sqlQuery = "SELECT * FROM food WHERE shrt_desc % '"+searchWord+"'"
+	
+	pool.query(sqlQuery)
+		.then((response) => res.send(response.rows))
+		.catch((err) => console.error('Error executing query', err.stack))
+})
+app.get('/api/collection/:searchWord', (req, res) => {
+	var searchWord = req.params.searchWord
+	console.log(searchWord)
+
+	// searchWord must be enclosed in single quotes
+	var sqlQuery = "SELECT * FROM food WHERE ndb_no IN ("+allCollections[searchWord].join()+")"
+
+	console.log(sqlQuery)
 	
 	pool.query(sqlQuery)
 		.then((response) => res.send(response.rows))
---
+++ public/allCollections.js
@@ -0,0 +1,6 @@
+
+var allCollections = {
+	highProtein : [10182, 22049]
+}
+
+export { allCollections }
